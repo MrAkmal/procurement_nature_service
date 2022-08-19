543af5a57ff84a7eda7fb1f5a0041ab7c9674dae
Asus <mr.akmal4555@mail.ru>
2022-08-19 16:15:26 +0500

    Initial commit

---
+++ src/main/java/com/example/procurement_nature_service/ProcurementNatureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.procurement_nature_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementNatureServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcurementNatureServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/procurement_nature_service/ProcurementNatureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.procurement_nature_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementNatureServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
