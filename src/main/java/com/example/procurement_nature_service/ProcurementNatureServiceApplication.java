3a85e30a339036b884768d3ab348c1f795f7af95
asadbekEngineere <asadbek.software.engineere@gmail.com>
2022-08-26 17:09:14 +0500

    swagger added

--- src/main/java/com/example/procurement_nature_service/ProcurementNatureServiceApplication.java
+++ src/main/java/com/example/procurement_nature_service/ProcurementNatureServiceApplication.java
@@ -1,11 +1,13 @@
 package com.example.procurement_nature_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
-//@OpenAPIDefinition(info = @Info(title = "Procurement Nature", version = "1.0", description = "Documentation APIs v1.0"))
+@OpenAPIDefinition(info = @Info(title = "Procurement Nature", version = "1.0", description = "Documentation APIs v1.0"))
 @EnableR2dbcRepositories
 public class ProcurementNatureServiceApplication {
 
